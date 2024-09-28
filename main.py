73210b6a1033f9511067cdb64056ad27b3385895
Gino Di Settimio <disettimiogino@gmail.com>
2024-09-28 16:08:00 -0300

    Consigna agregada

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#Escribe un programa que imprima los números del 1 al 100.
+#Pero para los múltiplos de 3, imprime la palabra "Fizz" en lugar del número, y para los múltiplos de 5, imprime "Buzz".
+#Para los números que son múltiplos de tanto 3 como 5, imprime "FizzBuzz".
+
 numbers = list(range(1,101))
 for n in numbers:
   if n % 15 == 0:
@@ -8,4 +12,3 @@ for n in numbers:
     n = ('Buzz')
 
   print(n)
-  
